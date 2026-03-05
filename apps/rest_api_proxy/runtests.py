44f7d685a789cc156d850d8e76b3fb73e9a70bc1
Aeliton G. Silva <aeliton@aeliton.com>
2026-03-04 23:04:55 -0300

    Allow run tests without manage.py
    This is in preparation to the removal of the django project of the
    repository.

---
+++ apps/rest_api_proxy/runtests.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import sys
+import django
+from django.conf import settings
+from django.test.utils import get_runner
+
+if not settings.configured:
+    settings.configure(
+        DATABASES={
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"
+            }
+        },
+        INSTALLED_APPS=[
+            "django.contrib.auth",
+            "django.contrib.contenttypes",
+        ],
+        REST_API_PROXY={
+            'HOST': 'http://from-settings.test',
+        },
+    )
+
+
+def run_tests():
+    django.setup()
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner()
+    failures = test_runner.run_tests(["tests"])
+    sys.exit(bool(failures))
+
+
+if __name__ == "__main__":
+    run_tests()
